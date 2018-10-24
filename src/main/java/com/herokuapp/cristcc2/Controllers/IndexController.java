99dfa05cee6847828695215ec8ea4801cb60b518
Cristiano Badalotti <cris.filth@hotmail.com>
2018-10-24 15:59:22 -0300

    tela de login

--- src/main/java/com/herokuapp/cristcc2/Controllers/IndexController.java
+++ src/main/java/com/herokuapp/cristcc2/Controllers/IndexController.java
@@ -2,16 +2,22 @@ package com.herokuapp.cristcc2.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
+	@RequestMapping(method =  RequestMethod.GET, path = "/entrar")
+	public String entrar( ) {
+		return "entrar";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/")
 	public String index() {
 		return "index";
 	}
 	
-	@RequestMapping("/home")
+	@RequestMapping(method = RequestMethod.GET, path = "/home")
 	public String indexHome() {
 		return "index";
 	}
--- src/main/java/com/herokuapp/cristcc2/Controllers/PosturaController.java
+++ src/main/java/com/herokuapp/cristcc2/Controllers/PosturaController.java
@@ -5,9 +5,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -46,9 +46,9 @@ public class PosturaController {
 		}
 	}
 	
-	@RequestMapping(value = "/deletarPostura", method = RequestMethod.GET)
-	public String deletarPostura(@RequestParam("codigoPostura") String codigo){
-		Postura postura = pr.findByCodigoPostura(Long.parseLong(codigo));
+	@RequestMapping("/cadastrarPostura/delete/{codigoPostura}") //@PathVariable Long id, RedirectAttributes redirectAttrs
+	public String deletarPostura(@PathVariable("codigoPostura") Long codigoPostura, RedirectAttributes redirectAttrs) {
+		Postura postura = pr.findByCodigoPostura(codigoPostura);
 		pr.delete(postura);
 		return "redirect:/cadastrarPostura";
 	}
