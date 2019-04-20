635c07b5ad297af205c36f73a55d504106ef17b2
Cristiano Badalotti <cris.filth@hotmail.com>
2019-04-19 22:56:06 -0300

    coreção no login

--- src/main/java/com/herokuapp/cristcc2/Segurança/WebSecurityConfig.java
+++ src/main/java/com/herokuapp/cristcc2/Segurança/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.herokuapp.cristcc2.Segurança;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -20,8 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/").permitAll().anyRequest()
-				.authenticated().and().formLogin().permitAll().and().logout()
+		http.csrf().disable().authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().authenticated()
+				.and().formLogin().permitAll().and().logout()
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 
